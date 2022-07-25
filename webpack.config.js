8793439817626dc17268053df9718c5a62b86abe
유정희(Junghee Yu)/WP팀/SKP <skp.1004816@sk.com>
2022-07-25 18:24:48 +0900

    로더 실습

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,20 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'images/[name][ext]?[hash]',
+        },
+      }
+    ],
+  },
 }
