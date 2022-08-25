695a21f7907a6a86fd04cbcdb9f7f44826dc70c1
wisheee <wishee.u@gmail.com>
2022-08-25 16:12:13 +0900

    webpack plugin 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    main: "./src/app.js"
+    main: './src/app.js'
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve("./dist")
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.css$/i,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader', 
+          'css-loader'
+        ],
       },
       {
-        test: /\.(png|jpg|svg|gif)$/,
-        loader: "url-loader",
-        options: {
-          name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'images/[name][ext]?[hash]',
+        },
       }
-    ]
-  }
+    ],
+  },
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
@@ -32,4 +41,24 @@ module.exports = {
    * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
    * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
    */
-};
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Author: ${childProcess.execSync('git config user.name')}
+      `
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
+      }
+    }),
+    ...(
+      process.env.NODE_ENV === 'production'
+      ? [ new MiniCssExtractPlugin() ]
+      : []
+    ),
+  ]
+}
